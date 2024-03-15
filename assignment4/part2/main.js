c6165f23eaa309fc680d7032eee71c9ad1e3714c
Tayyib180 <tayyib.azam@dcmail.ca>
2024-03-15 15:10:04 -0400

    added files to a4 p2

---
+++ assignment4/part2/main.js
@@ -0,0 +1,28 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+const imageNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+/* Declaring the alternative text for each image file */
+
+const alertnativeText = ["pic1", "pic2", "pic3", "pic4", "pic5"];
+
+/* Looping through images */
+
+for(let i = 0; i < imageNames.length; i++){
+    
+}
+
+//.addEventListener('click', result);
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
