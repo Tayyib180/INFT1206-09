8f12ee6ff34a36b47f6bfbb08c0a684a08c88ad9
Tayyib180 <tayyib.azam@dcmail.ca>
2024-03-15 15:29:33 -0400

    added button to change image dark to light

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -30,7 +30,19 @@ for(let i = 0; i < imageNames.length; i++){
 btn.addEventListener('click', function(){
     const btnClass = btn.getAttribute('class');
     if(btnClass === 'dark'){
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
         
     }
+    else{
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+
+
     
 })
