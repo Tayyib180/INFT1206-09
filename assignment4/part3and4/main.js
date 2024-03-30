07afe4db78402b81152da29ae9d795785b093091
Tayyib180 <tayyib.azam@dcmail.ca>
2024-03-30 19:38:41 -0400

    added the collision detect method

--- assignment4/part3and4/main.js
+++ assignment4/part3and4/main.js
@@ -135,6 +135,22 @@ class EvilCircle extends Shape {
     }
   }
 
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+          para.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
+
 }
 
 const balls = [];
